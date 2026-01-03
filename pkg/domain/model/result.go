078c7b27fc225108c51e378c2874c985962f142c
Masayoshi Mizutani <masayoshi.mizutani@dr-ubie.com>
2026-01-03 19:52:10 +0900

    fix(result.go): remove firestore tags from Scan struct fields for consistency in serialization

--- pkg/domain/model/result.go
+++ pkg/domain/model/result.go
@@ -8,13 +8,13 @@ import (
 )
 
 type Scan struct {
-	ID        types.ScanID   `bigquery:"id" firestore:"id" json:"id"`
-	Timestamp time.Time      `bigquery:"timestamp" firestore:"timestamp" json:"timestamp"`
-	GitHub    GitHubMetadata `bigquery:"github" firestore:"github" json:"github"`
-	Report    trivy.Report   `bigquery:"report" firestore:"report" json:"report"`
+	ID        types.ScanID   `bigquery:"id" json:"id"`
+	Timestamp time.Time      `bigquery:"timestamp" json:"timestamp"`
+	GitHub    GitHubMetadata `bigquery:"github" json:"github"`
+	Report    trivy.Report   `bigquery:"report" json:"report"`
 }
 
 type ScanRawRecord struct {
 	Scan
-	Timestamp int64 `bigquery:"timestamp" firestore:"timestamp" json:"timestamp"`
+	Timestamp int64 `bigquery:"timestamp" json:"timestamp"`
 }
